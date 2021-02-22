4cc77c14657b05339f310665bb7536aa492aa293
James Hayden <james@alumni.cs.utexas.edu>
2021-02-21 23:47:58 -0600

    Exit codes should have been 1 instead of -1

--- update-do.sh
+++ update-do.sh
@@ -1,16 +1,16 @@
 if [[ -z "$DOMAIN_NAME" ]]; then
   echo Expected environment variable DOMAIN_NAME.
-  exit -1
+  exit 1
 fi
 
 if [[ -z "$RECORD_ID" ]]; then
   echo Expected environment variable RECORD_ID.
-  exit -1
+  exit 1
 fi
 
 if [[ -z "$DO_TOKEN" ]]; then
   echo Expected environment variable DO_TOKEN.
-  exit -1
+  exit 1
 fi
 
 IP_ADDRESS=`curl -s http://ipaddr.io/ip`
